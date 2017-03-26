ac35e8fc33616aa75a1b5b057764cbb382278639
Matt Todd <matt.c.todd@gmail.com>
2017-03-26 18:49:45 -0500

    added angular to the sessions page

---
+++ app/assets/javascripts/session/app.js
@@ -0,0 +1,10 @@
+//= require core-js/client/shim.min.js
+//= require zone.js/dist/zone.js
+//= require rxjs/bundles/Rx.js
+//= require @angular/core/bundles/core.umd.js
+//= require @angular/common/bundles/common.umd.js
+//= require @angular/compiler/bundles/compiler.umd.js
+//= require @angular/platform-browser/bundles/platform-browser.umd.js
+//= require @angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js
+
+//= require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,5 @@
 
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( session/app.js )
 

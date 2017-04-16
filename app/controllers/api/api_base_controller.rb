c7cfaace8dc2b7542f2a9c2f8c9f8b917febef15
Matt Todd <matt.c.todd@gmail.com>
2017-04-16 12:48:41 -0500

    delintify

--- app/controllers/api/api_base_controller.rb
+++ app/controllers/api/api_base_controller.rb
@@ -1,9 +1,11 @@
-class Api::ApiBaseController < ActionController::Base
-  before_action :api_authenticate
+module Api
+  class ApiBaseController < ActionController::Base
+    before_action :api_authenticate
 
-  def api_authenticate
-    authenticate_or_request_with_http_token do |token, _|
-      ApiAccess.find_by(api_key: token).present?
+    def api_authenticate
+      authenticate_or_request_with_http_token do |token, _|
+        ApiAccess.find_by(api_key: token).present?
+      end
     end
   end
 end

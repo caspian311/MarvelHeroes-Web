4d02f710ddc59f48a94752cd2b47dfcf18a2504d
Matt Todd <matt.c.todd@gmail.com>
2017-04-16 12:47:49 -0500

    refactored to use the api base controllers auth

--- app/controllers/api/api_base_controller.rb
+++ app/controllers/api/api_base_controller.rb
@@ -2,10 +2,8 @@ class Api::ApiBaseController < ActionController::Base
   before_action :api_authenticate
 
   def api_authenticate
-    head :unauthorized unless authenticated?
-  end
-
-  def authenticated?
-    true
+    authenticate_or_request_with_http_token do |token, _|
+      ApiAccess.find_by(api_key: token).present?
+    end
   end
 end
